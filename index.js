2baf2bf081f7f8902d77fe2e6614f6c5cc8671f9
Dabuaf86 <dabuaf@gmail.com>
2023-07-18 16:02:20 -0300

    env commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const conectarDB = require('./config/db');
 const cors = require('cors');
-require('dotenv').config({ path: 'variables.env' });
+require('dotenv').config({ path: '.env' });
 
 // Crear el servidor
 const app = express();
